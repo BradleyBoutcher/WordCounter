5d2ae1c44d885d44752eb440ee2efe863c5d91e6
Bradley <bboutcher@vmware.com>
2019-09-03 18:28:12 -0400

    one line removal

--- WordCounter/src/main/java/com/bboutcher/FileReader.java
+++ WordCounter/src/main/java/com/bboutcher/FileReader.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Scanner;
 import java.util.concurrent.CompletableFuture;
